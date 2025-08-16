ed320f6ba5d8802c00ec2ba7f00cb001cd180854
Petter Salminen <kemikaze+github@gmail.com>
2025-08-16 23:49:53 +0200

    chore: bump versionCode to 2 and versionName to 1.1
    Updated the app's versionCode and versionName in `build.gradle.kts` to reflect a new release. These changes ensure proper versioning for distribution and tracking.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "se.araisan.stalk.app"
         minSdk = 34
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
